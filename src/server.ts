7344cbcccd6fb30a63a799b8ceaed142ac83aff0
yenthanh56 <57222518+yenthanh56@users.noreply.github.com>
2023-09-13 14:48:23 +0700

    update

--- src/server.ts
+++ src/server.ts
@@ -8,30 +8,6 @@ dotenv.config();
 const app: Express = express();
 const PORT = 3000;
 
-app.use(function (req: Request, res: Response, next: NextFunction) {
-	// Website you wish to allow to connect
-	// Website you wish to allow to connect
-	res.setHeader("Access-Control-Allow-Origin", "*");
-
-	// Request methods you wish to allow
-	res.setHeader(
-		"Access-Control-Allow-Methods",
-		"GET, POST, OPTIONS, PUT, PATCH, DELETE"
-	);
-
-	// Request headers you wish to allow
-	res.setHeader(
-		"Access-Control-Allow-Headers",
-		"X-Requested-With,content-type"
-	);
-
-	// Set to true if you need the website to include cookies in the requests sent
-	// to the API (e.g. in case you use sessions)
-	// res.setHeader("Access-Control-Allow-Credentials", true);
-
-	// Pass to next layer of middleware
-	next();
-});
 // middle
 app.use(cors());
 app.use(express.json());

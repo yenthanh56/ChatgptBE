ddc62d189cc1e4e32a2a95ff2aa7cdec3cf90430
yenthanh56 <57222518+yenthanh56@users.noreply.github.com>
2023-09-13 14:00:28 +0700

    update

--- src/server.ts
+++ src/server.ts
@@ -27,13 +27,13 @@ app.use(function (req: Request, res: Response, next: NextFunction) {
 
 	// Set to true if you need the website to include cookies in the requests sent
 	// to the API (e.g. in case you use sessions)
-	res.setHeader("Access-Control-Allow-Credentials", "true");
+	// res.setHeader("Access-Control-Allow-Credentials", true);
 
 	// Pass to next layer of middleware
 	next();
 });
 // middle
-app.use(cors({ origin: "*" }));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
